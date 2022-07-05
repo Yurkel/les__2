29af4ab76472b930c31e26471835db63462b1389
Yuriy <yuriy-k@ukr.net>
2022-07-05 16:13:39 +0300

    готово

---
+++ main.js
@@ -0,0 +1,28 @@
+const userName = prompt("Your name").trim();
+const userSurname = prompt("Your surname").trim();
+const userEmail = prompt("Your email")
+  .trim()
+  .toLocaleLowerCase()
+  .split(" ")
+  .join("");
+
+let messageEmail = `${userEmail}`;
+
+if (!userEmail.includes("@")) {
+  messageEmail = `not valid email <b>${userEmail}</b> (symbol @ not exist)`;
+} else if (userEmail.startsWith("@")) {
+  messageEmail = `not valid email <b>${userEmail}</b> (symbol @ find in first place)`;
+} else if (userEmail.endsWith("@")) {
+  messageEmail = `not valid email <b>${userEmail}</b> (symbol @ find in last place)`;
+}
+
+const userYear = +prompt("Your year of birth");
+const currentYear = new Date().getFullYear();
+const userAge = currentYear - userYear;
+
+document.write(`
+<ul>
+  <li>Full name: ${userName} ${userSurname}</li>
+  <li>Email: ${messageEmail}</li>
+  <li>Age: ${userAge}</li>
+</ul>`);
