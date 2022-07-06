53c9b3391e39d89fe4cb57f92ec6d8f60373c0dc
Yuriy <yuriy-k@ukr.net>
2022-07-06 13:25:12 +0300

    edit

--- main.js
+++ main.js
@@ -16,13 +16,13 @@ if (!userEmail.includes("@")) {
   messageEmail = `not valid email <b>${userEmail}</b> (symbol @ find in last place)`;
 }
 
-const userYear = +prompt("Your year of birth");
+const userYear = +prompt("Your year of birth").trim().split(" ").join("");
 const currentYear = new Date().getFullYear();
 const userAge = currentYear - userYear;
 
 document.write(`
 <ul>
-  <li>Full name: ${userName} ${userSurname}</li>
-  <li>Email: ${messageEmail}</li>
-  <li>Age: ${userAge}</li>
+<li>Full name: ${userName} ${userSurname}</li>
+<li>Email: ${messageEmail}</li>
+<li>Age: ${userAge}</li>
 </ul>`);
